9a154aa23bb2d3f16dc774bc4345eb03a16c0228
andreinikolabai <andreiplus2000@gmail.com>
2023-02-23 23:10:51 +0200

    update

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const n = prompt('Кількість чисел');
 const arr = [];
-let i;
 
-for (i = 0; i < n; i++) {
+for (let i = 0; i < n; i++) {
     arr.push(+prompt(`Введіть ${i+1} номер`))
-    arr.sort((a, b) => a - b);
 }
-arr.splice(2, 3, arr);
 
-alert('Масив з ' + n + ' чисел = [' + arr.join(', ') + ']')
+arr.sort((a, b) => a - b);
+
+alert(`Масив з ${n} чисел після сортування = [${arr.join(', ')}]`);
+arr.splice(2, 3);
+
+alert(`Масив з ${n} чисел після видалення елементів = [${arr.join(', ')}]`);
